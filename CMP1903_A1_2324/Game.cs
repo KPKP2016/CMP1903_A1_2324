05dcfb1fb593a3992b6a1eb49a32925ab9b8c05a
KPKP2016 <KPatelrandom@gmail.com>
2024-04-08 16:00:38 +0100

    Added SevensOut class and ThreeOrMore class. Deleted program class.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,40 +1,124 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CMP1903_A1_2324
 {
-
     internal class Game
     {
-
-        public int DiceGame()
+        public static bool isTesting = false;
+        public static void Main(string[] args)
         {
+            // creates play object and starts the game.
 
-            // 3 objects created from Die class.
-            Die die1 = new Die();
-            Die die2 = new Die();
-            Die die3 = new Die();
+            Console.WriteLine("SevensOut or ThreeOrMore? (1,2): ");
+            String gameChoice = Console.ReadLine();
 
-            // initiate roll
-            int roll1 = die1.Roll();
-            int roll2 = die2.Roll();
-            int roll3 = die3.Roll();
-
-            // does not print result if test is active
-            if (Program.isTesting == false)
+            while ((gameChoice != "1") && (gameChoice != "2"))
             {
-                Console.WriteLine("Die 1: " + roll1);
-                Console.WriteLine("Die 2: " + roll2);
-                Console.WriteLine("Die 3: " + roll3);
+                Console.WriteLine("SevensOut or ThreeOrMore? (1,2): ");
+                gameChoice = Console.ReadLine();
             }
 
-            // sum of 3 dice rolls
-            int RollTotal = roll1 + roll2 + roll3;
+            if (gameChoice == "1")
+            {
+                Game.SevensOut playSeven = new Game.SevensOut();
+                Console.WriteLine("Total: " + playSeven.DiceGame());
+            }
+            else if(gameChoice == "2")
+            {
+                Game.ThreeOrMore playThree = new Game.ThreeOrMore();
+                Console.WriteLine("points: " + playThree.DiceGame2());
+            }
+            Console.ReadKey();
 
-            // converts total to string so it can be printed
-            RollTotal.ToString();
-            return RollTotal;
+        }
+
+        // SevensOut game
+        internal class SevensOut
+        {
+            public int DiceGame()
+            {
+                Die die1 = new Die();
+                Die die2 = new Die();
+
+                int roll1 = die1.Roll();
+                int roll2 = die2.Roll();
+
+                int RollTotal = roll1 + roll2;
+
+                if (Game.isTesting == false)
+                {
+                    Console.WriteLine("Die 1: " + roll1);
+                    Console.WriteLine("Die 2: " + roll2);
+                }
+
+                if (RollTotal != 7)
+                {
+                    return RollTotal;
+                }
+                else if (die1 == die2)
+                {
+                    RollTotal = (roll1 + roll2) * 2;
+                    return RollTotal;
+                }
+                else
+                {
+                    Console.WriteLine("Your Total is 7, Game Ended.");
+                    return 0;
+                }
+            }
+        }
+
+        internal class ThreeOrMore
+        {
+            public int DiceGame2()
+            {
+                List<int> rolledList = new List<int>();
+                int Points = 0;
+
+                Die die1 = new Die();
+                Die die2 = new Die();
+                Die die3 = new Die();
+                Die die4 = new Die();
+                Die die5 = new Die();
+
+                int roll1 = die1.Roll();
+                int roll2 = die2.Roll();
+                int roll3 = die3.Roll();
+                int roll4 = die4.Roll();
+                int roll5 = die5.Roll();
+
+                rolledList.Add(roll1);
+                rolledList.Add(roll2);
+                rolledList.Add(roll3);
+                rolledList.Add(roll4);
+                rolledList.Add(roll5);
 
 
+
+                // do this
+
+                if (Game.isTesting == false)
+                {
+                    Console.WriteLine("Die 1: " + roll1);
+                    Console.WriteLine("Die 2: " + roll2);
+                    Console.WriteLine("Die 3: " + roll3);
+                    Console.WriteLine("Die 4: " + roll4);
+                    Console.WriteLine("Die 5: " + roll5);
+                }
+
+                if (roll1 == roll2)
+                {
+                    return 1;
+                }
+
+                else
+                {
+                    return 0;
+                }
+            }
         }
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -8,10 +8,10 @@ namespace CMP1903_A1_2324
         public Testing()
         {
             // testing is run before the final output
-            Program.isTesting = true;
+            Game.isTesting = true;
             TestDiceRollValue();
             TestDiceRollSum();
-            Program.isTesting = false;
+            Game.isTesting = false;
         }
         public void TestDiceRollValue()
         {
@@ -20,17 +20,17 @@ namespace CMP1903_A1_2324
             int TestVal = test.Roll();
 
             // Tests if die value is inside bounds
-            Debug.Assert(TestVal > 0 && TestVal < 7, "Dice value is out of bounds."); ;
+            Debug.Assert(TestVal > 0 && TestVal < 7, "Dice value is out of bounds."); 
         }
 
         public void TestDiceRollSum()
         {
             // Creates new TestSum object
-            Game testPlay = new Game();
+            Game.SevensOut testPlay = new Game.SevensOut();
             int TestSum = testPlay.DiceGame();
 
             // Tests if sum is inside bounds
-            Debug.Assert(TestSum > 2 && TestSum < 19, "Total sum of the rolled dice, is out of bounds");
+            Debug.Assert(TestSum > 1 && TestSum < 13, "Total sum of the rolled dice, is out of bounds");
         }
     }
 }
